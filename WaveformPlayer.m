3f721d6e80f086dd08098c202edd7742abea4196
Jan Willhaus <mail@janwillhaus.de>
2013-05-09 15:09:50 +0200

    I wanted a new color scheme for stereo, so here it is. :P

--- WaveformPlayer.m
+++ WaveformPlayer.m
@@ -93,7 +93,7 @@ function [hFigure, hWaveAxes, hOverviewAxes, stFuncHandles] = WaveformPlayer(szF
 %
 
 %--------------------------------------------------------------------------
-% VERSION 0.35
+% VERSION 0.35.1
 %   Author: Jan Willhaus (c) IHA @ Jade Hochschule
 %   applied licence see EOF
 %
@@ -147,6 +147,8 @@ function [hFigure, hWaveAxes, hOverviewAxes, stFuncHandles] = WaveformPlayer(szF
 %               in kHz and fixed a small bug resulting in 
 %               always showing a spectrogram when a slide
 %               action is performed. Other small fixes.
+%   Ver. 0.35.1 Fix: Audio playback won't lead to crash     09-May-2013     JW
+%               when changing view type
 
 %DEBUG
 %szFileName = 'TomShort.wav';
@@ -169,8 +171,8 @@ end
 vUpperAxesPos       = [ 0.05    0.45    0.90    0.50];
 vOverviewAxesPos    = [ 0.05    0.23    0.90    0.15];
 
-myColorsetFace      = [ 051/255 051/255 230/255; ...    % blue
-                        230/255 051/255 051/255; ...    % red
+myColorsetFace      = [ 061/255 129/255 136/255; ...    % blue
+                        223/255 104/255 098/255; ...    % red
                         051/255 230/255 051/255; ...    %
                         230/255 230/255 051/255; ...
                         230/255 051/255 230/255; ...
@@ -1225,8 +1227,12 @@ init();
                 set(hOverviewPos, ...
                     'XData', [CurrentPos CurrentPos]);
                 
-                set(hWavePos, ...
-                    'XData', [CurrentPos CurrentPos]);
+                if ~ishandle(hWavePos)
+                    createWavePosLine;
+                else
+                    set(hWavePos, ...
+                        'XData', [CurrentPos CurrentPos]);
+                end
                 
                 drawnow;
                 % SUPER INEFFICIENT! ALTERNATIVE?
