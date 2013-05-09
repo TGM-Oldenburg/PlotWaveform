8c33089bed541abf85beefdc2b91feea3bc3ce3b
Jan Willhaus <mail@janwillhaus.de>
2013-05-09 12:45:50 +0200

    Updated versioning

--- WaveformPlayer.m
+++ WaveformPlayer.m
@@ -93,7 +93,7 @@ function [hFigure, hWaveAxes, hOverviewAxes, stFuncHandles] = WaveformPlayer(szF
 %
 
 %--------------------------------------------------------------------------
-% VERSION 0.34
+% VERSION 0.34.1
 %   Author: Jan Willhaus (c) IHA @ Jade Hochschule
 %   applied licence see EOF
 %
@@ -139,6 +139,10 @@ function [hFigure, hWaveAxes, hOverviewAxes, stFuncHandles] = WaveformPlayer(szF
 %               Fix: Colormap depth works again and slider
 %               input is now even better verified in the
 %               zoom post action callback.
+%   Ver. 0.34.1 Fix: Moved the function handle (post zoom)  09-May-2013     JW
+%               a little further down, so the exection 
+%               starts *after* re-writing the plots. 
+%               (at suggestion of Julian Kahnert) 
 
 %DEBUG
 %szFileName = 'TomShort.wav';
